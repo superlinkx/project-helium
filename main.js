75672df3cfd5fbf72db9ea38d1cb611e87d3dacc
steven <steven@localhost>
2011-07-04 16:24:01 -0700

    Created background function

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var x = 0;
 var y = 0;
 var xDirection = 5;
 var yDirection = 5;
-var image = new Image();
-	image.src = "/projects/hydrogen/apps/helium/images/smiley.svg";
+var bg = new Image();
+	bg.src = "/projects/hydrogen/apps/helium/images/background.png";
 var canvas = null;
 var body = null;
 var context2D = null;
@@ -20,34 +20,15 @@ function init(){
 	context2D = canvas.getContext('2d');
 	canvas.width = w;
 	canvas.height = h;
-	context2D.drawImage(image, x, y);
 	setInterval(draw, 1000 / FPS);
+}
+
+function background(){
+	context2D.drawImage(bg,x,y)
+	y += 10
 }
 
 function draw(){
 	canvas.width = canvas.width;
-   	context2D.drawImage(image, x, y);
-
-    x += 1 * xDirection;
-    y += 1 * yDirection;
-
-    if (x >= width){
-		x = width;
-    	xDirection = -5;
-    }
-
-    else if (x <= 0){
-    	x = 0;
-        xDirection = 5;
-    }
-
-	if (y >= height){
-		y = height;
-       	yDirection = -5;
-	}
-
-	else if (y <= 0){
-       	y = 0;
-   		yDirection = 5;
-   	}
+	background;
 }
