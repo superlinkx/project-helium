806741b379a1c9cda95a88337524be02d67eaf3b
Steven Holms <superlinkx@gmail.com>
2011-07-16 20:16:00 -0700

    Lots of bug fixes and press-n-hold fire enabled

--- scripts/game.js
+++ scripts/game.js
@@ -26,60 +26,13 @@ function gameLoop(){
 		drawEnemy1();
 		playerDraw();
 		drawLaser();
+                laserFire();
 		hitTest();
 		shipCollision();
                 lvlchecker();
 	}	
 	scoreTotal();
 	game = setTimeout(gameLoop, 1000 / FPS);
-}
-
-function keyDown(e) {
-	if (e.keyCode == 68) rightKey = true;
-	else if (e.keyCode == 65) leftKey = true;
-	if (e.keyCode == 87) upKey = true;
-	else if (e.keyCode == 83) downKey = true;
-	if (e.keyCode == 74) lasers.push([px + 15, py - 30, 4, 20]);
-}
-
-function keyUp(e) {
-	if (e.keyCode == 68) rightKey = false;
-	else if (e.keyCode == 65) leftKey = false;
-	if (e.keyCode == 87) upKey = false;
-	else if (e.keyCode == 83) downKey = false;
-}
-
-function continueButton(e){
-	var cursorPos = getCursorPos(e);
-	if ((cursorPos.x > (w/2)-53 && cursorPos.x < (w/2)+47 && cursorPos.y > (h/2)+10 && cursorPos.y < (h/2)+50) || e.keyCode == 13){
-		alive = true;
-		lives = 3;
-		sc0re = 0;
-                speed = 3;
-		reset();
-		canvas.removeEventListener('click',continueButton,false);
-	}
-}
-
-function getCursorPos(e){
-	var x;
-	var y;
-	if (e.pageX || e.pageY){
-		x = e.pageX;
-		y = e.pageY;
-	}else{
-		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-		y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-	}
-	x -= canvas.offsetLeft;
-	y -= canvas.offsetTop;
-	var cursorPos = new cursorPosition(x,y);
-	return cursorPos;
-}
-
-function cursorPosition(x,y){
-	this.x = x;
-	this.y = y;
 }
 
 window.onload = init;
