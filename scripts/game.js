4eb8025d19e5f49bfa4822ea4534e394ab583dbc
Steven Holms <superlinkx@gmail.com>
2011-07-28 23:06:57 -0700

    Testing build app

---
+++ scripts/game.js
@@ -0,0 +1,38 @@
+/**
+* @preserve Copyright 2011
+* Project Helium and all its contents are copyrighted by Steven Holms <superlinkx>.
+* All rights reserved.
+* Do not distribute without permission.
+*/
+
+function init(){
+	canvas = document.getElementById('helium');	
+	ctx = canvas.getContext('2d');
+	canvas.width = w;
+	canvas.height = h;
+	ctx.drawImage(bg1, 0, 0);
+	document.addEventListener('keydown', keyDown, false);
+	document.addEventListener('keyup', keyUp, false);
+	document.addEventListener('click', gameStart, false);
+	gameLoop();
+}
+
+function gameLoop(){
+	ctx.clearRect(0,0,w,h)
+	backgroundDraw();
+	if (alive && gameStarted && lives > 0){
+		moveEnemy1();
+		moveLaser();
+		drawEnemy1();
+                laserFire();
+                drawLaser();
+		playerDraw();
+                hitTest();
+		shipCollision();
+                lvlchecker();
+	}	
+	scoreTotal();
+	game = setTimeout(gameLoop, 1000 / FPS);
+}
+
+window.onload = init;
---
+++ scripts/vars.js
@@ -0,0 +1,70 @@
+/**
+* @preserve Copyright 2011
+* Project Helium and all its contents are copyrighted by Steven Holms <superlinkx>.
+* All rights reserved.
+* Do not distribute without permission.
+*/
+
+//Root directory var
+if(!b) var b = "";
+// target frames per second
+var	FPS = 30,
+//height/width constants 
+	w = 240,
+	h = 480,
+	pw = 30,
+	ph = 30,
+	e1w = 30,
+	e1h = 30;
+//x-y variables
+	x = 0,
+	y = 0,
+	bgx = 0,
+	bg1y = -480,
+	bg2y = -1440,
+	px = (w/2)-15,
+	py = (h-30),
+	e1x = 25,
+	e1y = -45,
+//images and sprites
+	bg1 = new Image();
+	bg1.src = b+"images/background.png";
+var	bg2 = new Image();
+	bg2.src = b+"images/background.png";
+var	pSprite = new Image();
+	pSprite.src = b+"images/psprite.png";
+//keys
+var	upKey = false,
+	downKey = false,
+	rightKey = false,
+	leftKey = false,
+//enemies
+	enemy1Total = 3,
+	enemies = [],
+	enemy1Speed = 3,
+	e1xa = 50,		//amplitude
+	e1xf = (2*Math.PI)/240,	//frequency
+//lasers
+	lasers = [],
+        laserKey = false,
+        laserTime = 0,
+        laserWidth = 4,
+        laserCount = 8,
+        laserLimit = 8,
+        laserFireTracker = 0,
+//scoring
+	sc0re = 0,
+        scoreMult = 1,
+	alive = true,
+	lives = 3,
+	gameStarted = false,
+        lvl = 1,
+//canvas element vars
+	canvas = null,
+	body = null,
+	ctx = null;
+//enemy init
+for (var i = 0; i < enemy1Total; i++) {
+    e1x = (Math.random() * 200) + 25;
+    enemies.push([e1x, e1y, e1w, e1h, enemy1Speed, e1x]);
+}
