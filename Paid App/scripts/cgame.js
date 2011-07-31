e7713fa01156e670ca5bcfd3c18083080ab5409a
Steven Holms <superlinkx@gmail.com>
2011-07-30 18:22:33 -0700

    Updated manifest for free app

--- Paid App/scripts/cgame.js
+++ Paid App/scripts/cgame.js
@@ -4,5 +4,5 @@
  All rights reserved.
  Do not distribute without permission.
 */
-function init(){canvas=document.getElementById("helium");ctx=canvas.getContext("2d");canvas.width=w;canvas.height=h;document.addEventListener("keydown",keyDown,!1);document.addEventListener("keyup",keyUp,!1);document.addEventListener("click",gameStart,!1);gameLoop()}
+function init(){canvas=document.getElementById("helium");ctx=canvas.getContext("2d");canvas.width=w;canvas.height=h;explodeEffect.volume=0;laserEffect.volume=0;explodeEffect.play();explodeEffect.pause();laserEffect.play();laserEffect.pause();explodeEffect.volume=1;laserEffect.volume=1;document.addEventListener("keydown",keyDown,!1);document.addEventListener("keyup",keyUp,!1);document.addEventListener("click",gameStart,!1);gameLoop()}
 function gameLoop(){ctx.clearRect(0,0,w,h);backgroundDraw();alive&&gameStarted&&lives>0&&(moveEnemy1(),moveLaser(),drawEnemy1(),laserFire(),drawLaser(),playerDraw(),hitTest(),shipCollision(),lvlchecker());scoreTotal();game=setTimeout(gameLoop,1E3/FPS)}window.onload=init;
