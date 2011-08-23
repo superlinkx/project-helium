23ff6598adc56d4253c085f734b97447e18785eb
Steven Holms <superlinkx@gmail.com>
2011-08-23 01:24:44 -0700

    Minor changes

---
+++ Source/Rewrite/engine.js
@@ -0,0 +1,42 @@
+function Player(){
+    //player superclass
+    //begin properties
+    //end properties
+    //begin methods
+    //end methods
+}
+function HeliumClass1(){
+    //helium class 1 class
+    //begin properties
+    //end properties
+    //begin methods
+    //end methods
+}
+function Enemy(){
+    //enemy superclass
+    //begin properties
+    //end properties
+    //begin methods
+    //end methods
+}
+function Enemy1(){
+    //First enemy class
+    //begin properties
+    //end properties
+    //begin methods
+    //end methods
+}
+function Draw(){
+    //draw superclass
+    //begin properties
+    //end properties
+    //begin methods
+    //end methods
+}
+function Scoreboard(){
+    //scoreboard class
+    //begin properties
+    //end properties
+    //begin methods
+    //end methods
+}
