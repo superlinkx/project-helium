7e0686352e5568afe556a9a4834323d1dd84c8e1
Steven Holms <superlinkx@gmail.com>
2011-09-24 22:03:43 -0700

    Added local fonts, increased canvas size to 480x800, added more level attributes, tweaked interface to work with new size, other minor tweaks.

---
+++ Source/Source/DummyApp/scripts/game.js
@@ -0,0 +1,137 @@
+/**
+* @preserve Copyright 2011
+* Project Helium and all its contents are copyrighted by Steven Holms <superlinkx>.
+* All rights reserved.
+* Do not distribute without permission.
+*/
+function lvlchecker(){
+	switch(sc0re){
+		case 0: lvl = 1;
+			break;
+		case 1000: lvl = 2;
+			break;
+		case 2000: lvl = 3;
+			break;
+		case 3000: lvl = 4;
+			break;
+		case 4000: lvl = 5;
+			break;
+		case 5000: lvl = 6;
+			break;
+		case 6000: lvl = 7;
+			break;
+		case 7000: lvl = 8;
+			break;
+		case 8000: lvl = 9;
+			break;
+		case 9000: lvl = 10;
+			break;
+		default: break;
+	}
+	switch(lvl){
+		case 1:
+			enemy1Speed = 3;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 2:
+			enemy1Speed = 4;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 3:
+			enemy1Speed = 5;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 4:
+			enemy1Speed = 6;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 5:
+			enemy1Speed = 7;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 6:
+			enemy1Speed = 8;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 7:
+			enemy1Speed = 9;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 8:
+			enemy1Speed = 10;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 9:
+			enemy1Speed = 11;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+		case 10:
+			enemy1Speed = 12;
+			enemy1Total = 3;
+			sc0reMult = 1;
+			break;
+	}
+}
+function init(){
+	canvas = document.getElementById('helium');	
+	ctx = canvas.getContext('2d');
+	canvas.width = w;
+	canvas.height = h;
+        explodeEffect.volume=0;
+        laserEffect.volume=0;
+        explodeEffect.play();
+        explodeEffect.pause();
+        laserEffect.play();
+        laserEffect.pause();
+        explodeEffect.volume=1;
+        laserEffect.volume=1;
+	document.addEventListener('keydown', keyDown, false);
+	document.addEventListener('keyup', keyUp, false);
+	canvas.addEventListener('click', gameStart, false);
+        var topScore = document.getElementById('topScore');
+        var lastScore = document.getElementById('lastScore');
+        if(localStorage["topScore"]){
+            topScore.innerHTML = 'Your Highest Score Ever Was: '+localStorage["topScore"];
+        }
+        if(localStorage["lastScore"]){
+            lastScore.innerHTML = 'Your Latest Score Was: '+localStorage["lastScore"];
+        }
+	//enemy init
+	for (var i = 0; i < enemy1Total; i++) {
+	    path = randomPath();
+	    e1x = currentPath(path);
+	    enemies.push([e1x, e1y, e1w, e1h, enemy1Speed, e1x]);
+	}
+	gameLoop();
+}
+function gameLoop(){
+	ctx.clearRect(0,0,w,h)
+	backgroundDraw();
+	if (alive && gameStarted && lives > 0){
+		moveEnemy1();
+		moveLaser();
+		drawEnemy1();
+                laserFire();
+                drawLaser();
+		playerDraw();
+                hitTest();
+		shipCollision();
+                lvlchecker();
+	}else if (!gameStarted){
+		intro();
+	}else if (!alive){
+		gameOver();
+	}
+	scoreboard();
+	game = window.setTimeout(gameLoop, 1000 / FPS);
+}
+window.onload = init;
---
+++ Source/Source/DummyApp/scripts/vars.js
@@ -0,0 +1,70 @@
+/**
+* @preserve Copyright 2011
+* Project Helium and all its contents are copyrighted by Steven Holms <superlinkx>.
+* All rights reserved.
+* Do not distribute without permission.
+*/
+//Root directory var
+if(!b) var b = "";
+//error and debugging
+var err;
+// target frames per second
+var	FPS = 30;
+//height/width constants 
+var	w = 240;
+var	h = 480;
+var	pw = 30,
+	ph = 30,
+	e1w = 30,
+	e1h = 30;
+//x-y variables
+	x = 0,
+	y = 0,
+	bgpos = 0,
+	px = (w/2)-(pw/2),
+	py = (h-ph),
+	e1x = 25,
+	e1y = -45,
+//sprites and sounds
+        laserEffect = new Audio(b+"sound/laserpulse.wav"),
+        explodeEffect = new Audio(b+"sound/explode.wav");
+	var pSprite = new Image();
+	pSprite.src = b+"images/heliumclass1.png";
+        var e1Sprite = new Image();
+        e1Sprite.src = b+"images/enemy1.png"
+//keys
+    var	upKey = false,
+	downKey = false,
+	rightKey = false,
+	leftKey = false,
+        enterKey = false,
+        gamePaused = false,
+//enemies
+	enemies = [],
+	enemy1Speed = 3,
+	enemy1Total = 3,
+        path = null,
+        pathSize = null,
+	e1xa = 50,		//amplitude
+	e1xf = (2*Math.PI)/240,	//frequency
+//lasers
+	lasers = [],
+        laserKey = false,
+        laserTime = 0,
+        laserWidth = 4,
+        laserCount = 4,
+        laserLimit = 4,
+        laserFireTracker = 0,
+        laserTimeout = 20,
+//scoring
+	sc0re = 0,
+	sc0reMult = 1,
+	alive = true,
+	lives = 3,
+	gameStarted = false,
+        lvl = 1,
+        storageCalled = false,
+//canvas element vars
+	canvas = null,
+	body = null,
+	ctx = null;
