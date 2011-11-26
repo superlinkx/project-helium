e7232b2ccb1698f084837485ae506247196792fa
Steven Holms <superlinkx@gmail.com>
2011-11-26 06:23:47 -0800

    Updated tree with blank directories for future use.

--- Source/DummyApp/scripts/engine.js
+++ Source/DummyApp/scripts/engine.js
@@ -1,17 +1,19 @@
-/**
-* @preserve Copyright 2011
-* Project Helium and all its contents are copyrighted by Steven Holms <superlinkx>.
-* All rights reserved.
-* Do not distribute without permission.
+/*
+*@preserve Copyright 2011 Steven Holms <superlinkx@gmail.com>
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
 */
 //Begin Level
-function getLevelData(){
-    var request = new XMLHttpRequest();
-    request.open('GET', 'levels.json', false); 
-    request.send(null);
-    if (request.status == 200)
-	eval(request.responseText);
-}
 //End Level
 //Begin Background
 function backgroundDraw(){
@@ -31,87 +33,209 @@ function backgroundDraw(){
 }
 //End Background
 //Begin Player
+function Player(x,y){
+    this.x = x;
+    this.y = y;
+    this.w = 30;
+    this.h = 30;
+}
 function playerDraw(){
-    if (rightKey) px += 5;
-    else if (leftKey) px -= 5;
-    if (upKey) py -= 5;
-    else if (downKey) py += 5;
+    if (rightKey) player.x += 5;
+    if (leftKey) player.x -= 5;
+    if (upKey) player.y -= 5;
+    if (downKey) player.y += 5;
     playerBounds();
-    ctx.drawImage(pSprite, px, py);
+    ctx.drawImage(pSprite, player.x, player.y);
 }
 
 function playerBounds(){
-    if (px > (w-31)){
-	px = (w-30);
+    if (player.x > (w-31)){
+    	player.x = (w-30);
     }
-    if (px < 1){
-    	px = 0;
+    if (player.x < 1){
+    	player.x = 0;
     }
-    if(py > (h-31)){
-	py = (h-30);
+    if(player.y > (h-31)){
+    	player.y = (h-30);
     }
-    if (py < 1){
-	py = 0;
+    if (player.y < 1){
+    	player.y = 0;
     }
 }
 function shipCollision(){
-    var pxw = px + pw,
-    pyh = py + ph;
+    var pxw = player.x + player.w,
+    pyh = player.y + player.h;
     for (var i = 0; i < enemies.length; i++){
-        if((px >= enemies[i][0] && px <= (enemies[i][0] + e1w)) || ((px + pw) >= enemies[i][0] && (px + pw) <= (enemies[i][0] + e1w))){
-            if((py >= enemies[i][1] && py <= (enemies[i][1] + e1h)) || ((py + ph) >= enemies[i][1] && (py + ph) <= (enemies[i][1] + e1h))){
-                explodeEffect.pause();
-                explodeEffect.currentTime=0;
-                explodeEffect.play();
+        if((player.x >= enemies[i].x && player.x <= (enemies[i].x + enemies[i].w)) || ((player.x + player.w) >= enemies[i].x && (player.x + player.w) <= (enemies[i].x + enemies[i].w))){
+            if((player.y >= enemies[i].y && player.y <= (enemies[i].y + enemies[i].h)) || ((player.y + player.h) >= enemies[i].y && (player.y + player.h) <= (enemies[i].y + enemies[i].h))){
                 checkLives();
             }
-	}
+        }
     }
 }
 //End Player
 //Begin Enemy
-function drawEnemy1(){
+function Enemy(x,y,w,h,speed,initx,type){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.speed = speed;
+    this.initx = initx;
+    this.type = type;
+    this.fired = 0;
+}
+function drawEnemy(){
     for (var i = 0; i < enemies.length; i++) {
-        ctx.drawImage(e1Sprite,enemies[i][0],enemies[i][1]);
+	switch (enemies[i].type){
+	    case 1: ctx.drawImage(e1Sprite,enemies[i].x,enemies[i].y);
+		    break;
+	    case 2: ctx.drawImage(e2Sprite,enemies[i].x,enemies[i].y);
+		    break;
+	    default: ctx.drawImage(e1Sprite,enemies[i].x,enemies[i].y);
+	}
+    }
+}
+function enemyFire(){
+    for (var i = 0; i < enemies.length; i++) {
+	switch (enemies[i].type){
+	    case 1: break;
+	    case 2:
+		if(!enemies[i].fired){
+		   enemyLasers.push(new Laser(enemies[i].x + (enemies[i].w/2 - laserWidth/2), enemies[i].y, laserWidth, 20));
+		   enemies[i].fired = 1;
+		   break;
+		}
+	    default: break;
+	}
+    } 
+}
+function drawEnemyLaser(){
+    if (enemyLasers.length){
+	for (var i=0; i < enemyLasers.length; i++){
+	    var laserGradient = ctx.createLinearGradient(enemyLasers[i].x,enemyLasers[i].y,enemyLasers[i].x,enemyLasers[i].y + 20);
+	    laserGradient.addColorStop(0,'rgba(255,0,0,0.2)');
+            laserGradient.addColorStop(1,'rgba(255,0,0,0.8)')
+            ctx.fillStyle = laserGradient;
+	    ctx.fillRect(enemyLasers[i].x,enemyLasers[i].y,enemyLasers[i].w,enemyLasers[i].h);
+	}
+    }
+}
+function moveEnemyLaser(){
+    for(var i = 0; i < enemyLasers.length; i++){
+	enemyLasers[i].y += 10
+	if (enemyLasers[i].y > 810){
+	    enemyLasers.splice(i,1);
+	}
+    }
+}
+function randomType(){
+    var random = Math.ceil(Math.random() * 2);
+    if(random == 0){
+	random = 1;
+    }
+    return random;
+}
+function typeSpeed(type){
+    switch(type){
+	case 1: return enemy1Speed;
+		break;
+	case 2: return enemy2Speed;
+		break;
+	default: return enemy1Speed;
+		break;
     }
 }
 function randomPath(){
-    var random = Math.ceil(Math.random() * 10);
+    var random = Math.ceil(Math.random() * 20);
     return random;
 }
 function currentPath(path){
     switch(path){
-	case 1: pathSize = (w/10);
+	case 1: pathSize = (w/20);
 	    break;
-	case 2: pathSize = (w/10)*2;
+	case 2: pathSize = (w/20)*2;
 	    break;
-	case 3: pathSize = (w/10)*3;
+	case 3: pathSize = (w/20)*3;
 	    break;
-	case 4: pathSize = (w/10)*4;
+	case 4: pathSize = (w/20)*4;
 	    break;
-	case 5: pathSize = (w/10)*5;
+	case 5: pathSize = (w/20)*5;
 	    break;
-	case 6: pathSize = (w/10)*6;
+	case 6: pathSize = (w/20)*6;
 	    break;
-	case 7: pathSize = (w/10)*7;
+	case 7: pathSize = (w/20)*7;
 	    break;
-	case 8: pathSize = (w/10)*8;
+	case 8: pathSize = (w/20)*8;
 	    break;
-	case 9: pathSize = (w/10)*9;
+	case 9: pathSize = (w/20)*9;
 	    break;
-	case 10: pathSize = (w/10)*10;
+	case 10: pathSize = (w/20)*10;
+	    break;
+	case 11: pathSize = (w/20)*11;
+	    break;
+	case 12: pathSize = (w/20)*12;
+	    break;
+	case 13: pathSize = (w/20)*13;
+	    break;
+	case 14: pathSize = (w/20)*14;
+	    break;
+	case 15: pathSize = (w/20)*15;
+	    break;
+	case 16: pathSize = (w/20)*16;
+	    break;
+	case 17: pathSize = (w/20)*17;
+	    break;
+	case 18: pathSize = (w/20)*18;
+	    break;
+	case 19: pathSize = (w/20)*19;
+	    break;
+	case 20: pathSize = (w/20)*20;
 	    break;
 	default: return err;
     }
     return pathSize;
 }
-function moveEnemy1(){
+function moveEnemy(){
     for (var i = 0; i < enemies.length; i++) {
-	enemies[i][0] = e1xa*Math.sin((e1xf*enemies[i][1]))+enemies[i][5];
-	if (enemies[i][1] < h) {
-	    enemies[i][1] += enemies[i][4];
-	} else if (enemies[i][1] > h - 1) {
-	    enemies[i][1] = -45;
+	switch(enemies[i].type){
+	    case 1:
+		enemies[i].x = e1xa*Math.sin((e1xf*enemies[i].y))+enemies[i].initx;
+		if (enemies[i].y < h) {
+		    enemies[i].y += enemies[i].speed;
+		} else if (enemies[i].y > h - 1) {
+		    enemies[i].y = -45;
+		}
+		break;
+	    case 2:
+		if (enemies[i].y < h){
+		    enemies[i].y += enemies[i].speed;
+		} else if (enemies[i].y > h - 1){
+		    enemies[i].y = -45;
+		}
+		break;
+	    default:
+	    	enemies[i].x = e1xa*Math.sin((e1xf*enemies[i].y))+enemies[i].initx;
+		if (enemies[i].y < h) {
+		    enemies[i].y += enemies[i].speed;
+		} else if (enemies[i].y > h - 1) {
+		    enemies[i].y = -45;
+		}
+	}
+    }
+}
+function enemyLaserTest(){
+    var remove = false;
+    for (var i = 0; i < enemyLasers.length; i++){
+        if (enemyLasers[i].x >= player.x && enemyLasers[i].x <= (player.x + player.w) || ((enemyLasers[i].x + enemyLasers[i].w) >= player.x && (enemyLasers[i].x + enemyLasers[i].w) <= (player.x + player.w))){
+	    if (enemyLasers[i].y >= player.y && enemyLasers[i].y <= (player.y + player.h)){
+		remove = true;
+		checkLives();
+	    }
+	}
+	if (remove == true){
+	    enemyLasers.splice(i,1);
+	    remove = false;
 	}
     }
 }
@@ -119,17 +243,17 @@ function hitTest(){
     var remove = false;
     for (var i = 0; i < lasers.length; i++){
 	for(var j = 0; j < enemies.length; j++){
-            if (lasers[i][0] >= enemies[j][0] && lasers[i][0] <= (enemies[j][0] + e1w) || ((lasers[i][0] + laserWidth) >= enemies[j][0] && (lasers[i][0] + laserWidth) <= (enemies[j][0] + e1w))){
-		if (lasers[i][1] >= enemies[j][1] && lasers[i][1] <= (enemies[j][1] + e1h)){
-                    explodeEffect.pause();
-                    explodeEffect.currentTime = 0;
-                    explodeEffect.play();
+            if (lasers[i].x >= enemies[j].x && lasers[i].x <= (enemies[j].x + enemies[j].w) || ((lasers[i].x + lasers[i].w) >= enemies[j].x && (lasers[i].x + lasers[i].w) <= (enemies[j].x + enemies[j].w))){
+		if (lasers[i].y >= enemies[j].y && lasers[i].y <= (enemies[j].y + enemies[j].h)){
+		    enemyKilled += 1;
                     remove = true;
                     enemies.splice(j,1);
 		    sc0re += (10*sc0reMult);
 		    path = randomPath();
 		    e1x = currentPath(path);
-		    enemies.push([e1x, -45, e1w, e1h, enemy1Speed, e1x]);
+		    type = randomType();
+		    var speed = typeSpeed(type);
+		    enemies.push(new Enemy(e1x, -45, e1w, e1h, speed, e1x, type));
 		}
             }
 	}
@@ -141,23 +265,26 @@ function hitTest(){
 }
 //End Enemy
 //Begin Laser
+function Laser(x,y,w,h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+}
 function drawLaser(){
     if (lasers.length){
 	for (var i=0; i < lasers.length; i++){
-	    var laserGradient = ctx.createLinearGradient(lasers[i][0],lasers[i][1],lasers[i][0],lasers[i][1] + 20);
+	    var laserGradient = ctx.createLinearGradient(lasers[i].x,lasers[i].y,lasers[i].x,lasers[i].y + 20);
 	    laserGradient.addColorStop(0,'rgba(255,0,0,0.8)');
             laserGradient.addColorStop(1,'rgba(255,0,0,0.2)')
             ctx.fillStyle = laserGradient;
-	    ctx.fillRect(lasers[i][0],lasers[i][1],lasers[i][2],lasers[i][3]);
+	    ctx.fillRect(lasers[i].x,lasers[i].y,lasers[i].w,lasers[i].h);
 	}
     }
 }
 function laserFire(){
     if(laserKey == true && laserTime >= 5 && laserCount > 0){
-        laserEffect.pause();
-        laserEffect.currentTime=0;
-        laserEffect.play();
-        lasers.push([px + (pw/2 - laserWidth/2), py, laserWidth, 20]);
+        lasers.push(new Laser(player.x + (player.w/2 - laserWidth/2), player.y, laserWidth, 20));
         laserTime = 0;
         laserCount--;
     }
@@ -169,9 +296,9 @@ function laserFire(){
 }
 function moveLaser(){
     for(var i = 0; i < lasers.length; i++){
-	if (lasers[i][1] > -11){
-	    lasers[i][1] -= 10;
-	} else if (lasers[i][1] < -10){
+	if (lasers[i].y > -11){
+	    lasers[i].y -= 10;
+	} else if (lasers[i].y < -10){
 	    lasers.splice(i,1);
 	}
     }
@@ -182,48 +309,52 @@ function intro(){
     ctx.fillStyle = 'rgba(255,255,255,0.7)';
     ctx.fillRect(0,0,w,h);
     ctx.fillStyle = '#f00';
-    ctx.font = 'bold 32px Arial';
-    ctx.fillText('Project Helium', w/2 - 110, h/2);
-    ctx.font = 'bold 20px Arial';
-    ctx.fillText('Click to Play', w/2 -56, h/2 + 30);
-    ctx.fillText('Use WASD to move', w/2-100, h/2 + 60);
-    ctx.fillText('Use the j key to shoot', w/2-100, h/2+90);
+    ctx.font = '54px VT323';
+    ctx.fillText('Project Helium', w/2 - 150, h/2);
+    ctx.font = '36px VT323';
+    ctx.fillText('Press Enter to Play', w/2 - 135, h/2 + 50);
+    ctx.fillText('Use arrow keys to move', w/2 - 160, h/2 + 90);
+    ctx.fillText('Use space to shoot', w/2 - 125, h/2+130);
 }
 function gameOver(){
     lives = 0;
     ctx.fillStyle = 'rgba(255,255,255,0.7)';
     ctx.fillRect(0,0,w,h);
     ctx.fillStyle = '#f00'
-    ctx.fillText('Game Over!', (w/2)-55, h/2);
-    ctx.fillText('Press Enter to Continue...',(w/2)-110,(h/2)+80);
-    ctx.fillRect((w/2)-53, (h/2)+ 10,100,40);
-    ctx.fillStyle = '#000';
-    ctx.fillText('Continue?', (w/2)-48, (h/2)+35);
+    ctx.fillText('Game Over!', (w/2)-60, h/2);
+    ctx.fillText('Press Enter to Continue...',(w/2)-130,(h/2)+80);
     if(!storageCalled) updateStorage();
-	canvas.addEventListener('click',continueButton,false);
-    if(enterKey){
-        alive = true;
-        lives = 3;
+    if (enterKey){
+	alive = true;
+	lives = 3;
         sc0re = 0;
-        speed = 3;
-        reset();
-        canvas.removeEventListener('click',continueButton,false);
+	enemyKilled = 0
+        enemy1Speed = 3;
+	enemy2Speed = 4;
+	reset();
+    }
+}
+function fullscreen(){
+    if (document.getElementById("helium").webkitRequestFullScreen){
+        document.getElementById("helium").webkitRequestFullScreen();
+    }else{
+        alert('Browser not supported. You need to install the latest WebKit (for example Chrome 15) in order to use the full-screen API.');
     }
 }
 //End Menu
 //Begin Score
 function scoreboard(){
-    ctx.font = 'bold 18px Arial';
+    ctx.font = '26px VT323';
     ctx.fillStyle = 'rgba(255,255,255,0.8)';
     ctx.fillRect(0,0,w,60)
     ctx.fillStyle = '#f00';
     ctx.fillText('Score: ', 10, 55);
-    ctx.fillText(sc0re, 70, 55);
-    ctx.fillText('Lives:', 10, 30);
-    ctx.fillText(lives, 68, 30);
-    ctx.fillText('Level:', (w-80), 30);
-    ctx.fillText(lvl, (w-20), 30);
-    ctx.fillText('Shots:', (w-80), 55);
+    ctx.fillText(sc0re, 170, 55);
+    ctx.fillText('Lives:', 10, 20);
+    ctx.fillText(lives, 170, 20);
+    ctx.fillText('Level:', (w-170), 20);
+    ctx.fillText(lvl, (w-20), 20);
+    ctx.fillText('Shots:', (w-170), 55);
     ctx.fillText(laserCount, (w-20), 55);
 }
 //End Score
@@ -241,12 +372,14 @@ function reset(){
     lasers.splice(0,lasers.length);
     laserFireTracker = 0;
     laserCount = 4;
-    px = (w/2) - 15, py = h - 30, pw = 30, ph = 30;
+    player.x = (w/2) - 15, player.y = h - 30, player.w = 30, player.h = 30;
     enemies.splice(0,enemies.length);
-    for(i=0;i<enemy1Total;i++){
+    for(i=0;i<enemyTotal;i++){
 	path = randomPath();
 	e1x = currentPath(path);
-	enemies.push([e1x, -45, e1w, e1h, enemy1Speed, e1x]);
+	type = randomType();
+	var speed = typeSpeed(type);
+	enemies.push(new Enemy(e1x, -45, e1w, e1h, speed, e1x, type));
     }
 }
 function pauseGame(){
@@ -256,10 +389,10 @@ function pauseGame(){
         ctx.fillStyle = 'rgba(255,255,255,0.7)'
         ctx.fillRect(0,0,w,h);
         ctx.fillStyle = '#f00';
-        ctx.font = 'bold 32px Arial';
-        ctx.fillText('Paused', w/2-56,h/2);
-        ctx.font = 'bold 20px Arial';
-        ctx.fillText('Press Esc to Continue',w/2-105,h/2+30)
+        ctx.font = '54px VT323';
+        ctx.fillText('Paused', w/2-70,h/2);
+        ctx.font = '36px VT323';
+        ctx.fillText('Press p to Continue',w/2-160,h/2+60)
     }else if(gamePaused){
         game = window.setTimeout(gameLoop, 1000/FPS);
         gamePaused = false;
@@ -284,59 +417,25 @@ function updateStorage(){
     lastScore.innerHTML = 'Your Latest Score Was: '+localStorage["lastScore"];
     storageCalled = true;
 }
-function Level(){
-    this.enemy1Speed;
-    this.enemy1Total;
-    this.sc0reMult;
-}
 //End Engine
 //Begin Controls
 function keyDown(e) {
-    if (e.keyCode == 68) rightKey = true;
-    else if (e.keyCode == 65) leftKey = true;
-    if (e.keyCode == 87) upKey = true;
-    else if (e.keyCode == 83) downKey = true;
-    if (e.keyCode == 74) laserKey = true;
+    e.preventDefault();
+    if (e.keyCode == 39) rightKey = true;
+    else if (e.keyCode == 37) leftKey = true;
+    if (e.keyCode == 38) upKey = true;
+    else if (e.keyCode == 40) downKey = true;
+    if (e.keyCode == 32) laserKey = true;
     if (e.keyCode == 13) enterKey = true;
-    if (e.keyCode == 27) pauseGame();
+    if (e.keyCode == 80) pauseGame();
 }
 function keyUp(e) {
-    if (e.keyCode == 68) rightKey = false;
-    else if (e.keyCode == 65) leftKey = false;
-    if (e.keyCode == 87) upKey = false;
-    else if (e.keyCode == 83) downKey = false;
-    if (e.keyCode == 74) laserKey = false;
+    e.preventDefault();
+    if (e.keyCode == 39) rightKey = false;
+    else if (e.keyCode == 37) leftKey = false;
+    if (e.keyCode == 38) upKey = false;
+    else if (e.keyCode == 40) downKey = false;
+    if (e.keyCode == 32) laserKey = false;
     if (e.keyCode == 13) enterKey = false;
-}
-function continueButton(e){
-    var cursorPos = getCursorPos(e);
-    if ((cursorPos.x > (w/2)-53 && cursorPos.x < (w/2)+47 && cursorPos.y > (h/2)+10 && cursorPos.y < (h/2)+50) || e.keyCode == 13){
-	alive = true;
-	lives = 3;
-        sc0re = 0;
-	enemyKilled = 0;
-        speed = 3;
-	reset();
-	canvas.removeEventListener('click',continueButton,false);
-    }
-}
-function getCursorPos(e){
-    var x;
-    var y;
-    if (e.pageX || e.pageY){
-	x = e.pageX;
-	y = e.pageY;
-    }else{
-        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-    	y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
-    x -= canvas.offsetLeft;
-    y -= canvas.offsetTop;
-    var cursorPos = new cursorPosition(x,y);
-    return cursorPos;
-}
-function cursorPosition(x,y){
-    this.x = x;
-    this.y = y;
 }
 //End Controls
--- Source/DummyApp/scripts/levels.js
+++ Source/DummyApp/scripts/levels.js
@@ -1,6 +1,102 @@
-/**
-* @preserve Copyright 2011
-* Project Helium and all its contents are copyrighted by Steven Holms <superlinkx>.
-* All rights reserved.
-* Do not distribute without permission.
+/*
+*@preserve Copyright 2011 Steven Holms <superlinkx@gmail.com>
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
 */
+function lvlchecker(){
+	switch(enemyKilled){
+		case 0: lvl = 1;
+			break;
+		case 100: lvl = 2;
+			break;
+		case 200: lvl = 3;
+			break;
+		case 300: lvl = 4;
+			break;
+		case 400: lvl = 5;
+			break;
+		case 500: lvl = 6;
+			break;
+		case 600: lvl = 7;
+			break;
+		case 700: lvl = 8;
+			break;
+		case 800: lvl = 9;
+			break;
+		case 900: lvl = 10;
+			break;
+		default: break;
+	}
+	switch(lvl){
+		case 1:
+			enemy1Speed = 3;
+                        enemy2Speed = 4;
+			enemyTotal = 8;
+			sc0reMult = 1;
+			break;
+		case 2:
+			enemy1Speed = 4;
+                        enemy2Speed = 5;
+			enemyTotal = 9;
+			sc0reMult = 1;
+			break;
+		case 3:
+			enemy1Speed = 5;
+                        enemy2Speed = 6;
+			enemyTotal = 10;
+			sc0reMult = 1;
+			break;
+		case 4:
+			enemy1Speed = 6;
+                        enemy2Speed = 7;
+			enemyTotal = 11;
+			sc0reMult = 1;
+			break;
+		case 5:
+			enemy1Speed = 7;
+                        enemy2Speed = 8;
+			enemyTotal = 12;
+			sc0reMult = 1;
+			break;
+		case 6:
+			enemy1Speed = 8;
+                        enemy2Speed = 9;
+			enemyTotal = 13;
+			sc0reMult = 1;
+			break;
+		case 7:
+			enemy1Speed = 9;
+                        enemy2Speed = 10;
+			enemyTotal = 14;
+			sc0reMult = 1;
+			break;
+		case 8:
+			enemy1Speed = 10;
+                        enemy2Speed = 11;
+			enemyTotal = 15;
+			sc0reMult = 1;
+			break;
+		case 9:
+			enemy1Speed = 11;
+                        enemy2Speed = 12;
+			enemyTotal = 16;
+			sc0reMult = 1;
+			break;
+		case 10:
+			enemy1Speed = 12;
+                        enemy2Speed = 13;
+			enemyTotal = 17;
+			sc0reMult = 1;
+			break;
+	}
+}
