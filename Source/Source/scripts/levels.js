a5be25a7a5a5638f73613e28aa825fe24c818088
Steven Holms <superlinkx@gmail.com>
2011-12-19 01:38:17 -0800

    Created new pause and restart images. Added pause and restart mouse controls. Also fixed a speed bug when the restart function was called previously. Did some css changes and simplified the button text.
    TODO: map restart function to key.
          see previous TODO

--- Source/Source/scripts/levels.js
+++ Source/Source/scripts/levels.js
@@ -1,19 +1,16 @@
 /**
-* 	Copyright 2011 Steven Holms <superlinkx@gmail.com>
-*
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
+*	Copyright 2011 Steven Holms <superlinkx@gmail.com>
+*	Licensed under the Apache License, Version 2.0 (the "License");
+*	you may not use this file except in compliance with the License.
+*	You may obtain a copy of the License at
+*	http://www.apache.org/licenses/LICENSE-2.0
+*	Unless required by applicable law or agreed to in writing, software
+*	distributed under the License is distributed on an "AS IS" BASIS,
+*	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*	See the License for the specific language governing permissions and
+*	limitations under the License.
 */
-function lvl_init() {
+function lvl_init(){
 	lvl1 = new Level([1,2,8,1,"rgb(0,0,0)","rgb(0,255,0)"]);
 	lvl2 = new Level([2,3,9,1,"rgb(0,0,0)","rgb(0,0,255)"]);
 	lvl3 = new Level([3,4,10,1,"rgb(0,0,0)","rgb(255,0,0)"]);
@@ -42,36 +39,36 @@ function lvlReader(){
 	bgline = currentLevel.bgline;
 }
 function lvlchecker(){
-	if (enemyKilled < 100) lvl = 1;
-	if (100 <= enemyKilled && enemyKilled < 200) lvl = 2;
-	if (200 <= enemyKilled && enemyKilled < 300) lvl = 3;
-	if (300 <= enemyKilled && enemyKilled < 400) lvl = 4;
-	if (400 <= enemyKilled && enemyKilled < 500) lvl = 5;
-	if (500 <= enemyKilled && enemyKilled < 600) lvl = 6;
-	if (600 <= enemyKilled && enemyKilled < 700) lvl = 7;
-	if (700 <= enemyKilled && enemyKilled < 800) lvl = 8;
-	if (800 <= enemyKilled && enemyKilled < 900) lvl = 9;
-	if (900 <= enemyKilled) lvl = 10;
+	if(enemyKilled < 100) lvl = 1;
+	if(100 <= enemyKilled && enemyKilled < 200) lvl = 2;
+	if(200 <= enemyKilled && enemyKilled < 300) lvl = 3;
+	if(300 <= enemyKilled && enemyKilled < 400) lvl = 4;
+	if(400 <= enemyKilled && enemyKilled < 500) lvl = 5;
+	if(500 <= enemyKilled && enemyKilled < 600) lvl = 6;
+	if(600 <= enemyKilled && enemyKilled < 700) lvl = 7;
+	if(700 <= enemyKilled && enemyKilled < 800) lvl = 8;
+	if(800 <= enemyKilled && enemyKilled < 900) lvl = 9;
+	if(900 <= enemyKilled) lvl = 10;
 	switch(lvl){
 		case 1: currentLevel = lvl1;
-				break;
+			break;
 		case 2: currentLevel = lvl2;
-				break;
+			break;
 		case 3: currentLevel = lvl3;
-				break;
+			break;
 		case 4: currentLevel = lvl4;
-				break;
+			break;
 		case 5: currentLevel = lvl5;
-				break;
+			break;
 		case 6: currentLevel = lvl6;
-				break;
+			break;
 		case 7: currentLevel = lvl7;
-				break;
+			break;
 		case 8: currentLevel = lvl8;
-				break;
+			break;
 		case 9: currentLevel = lvl9;
-				break;
+			break;
 		case 10: currentLevel = lvl10;
-				break;
+			break;
 	}
 }
